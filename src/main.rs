b303ec01f959a592e71a40086a18a619d71d57a4
FlorianWoelki <FlorianWoelki@gmx.de>
2024-04-18 09:36:43 +0200

    feat: add getting a list of issues

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use reqwest::header::USER_AGENT;
+use futures::future::BoxFuture;
+use reqwest::header::{ACCEPT, USER_AGENT};
 use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize, Serialize, Debug, PartialEq)]
@@ -8,15 +9,62 @@ struct User {
 
 #[derive(Deserialize, Serialize, Debug, PartialEq)]
 struct Issue {
+    number: usize,
+    title: String,
+}
+
+#[derive(Deserialize, Serialize, Debug, PartialEq)]
+struct IssueReaction {
     content: String,
     user: User,
 }
 
-async fn get_issue_reactions() -> Vec<Issue> {
+fn get_issues_wrapper(page: Option<usize>) -> BoxFuture<'static, Vec<Issue>> {
+    Box::pin(get_issues(page))
+}
+
+async fn get_issues(page: Option<usize>) -> Vec<Issue> {
+    let page = page.unwrap_or(1);
     let request_url = format!(
-        "https://api.github.com/repos/{owner}/{repo}/issues/76/reactions",
+        "https://api.github.com/repos/{owner}/{repo}/issues?state=open&page={page}",
         owner = "FlorianWoelki",
-        repo = "obsidian-iconize"
+        repo = "obsidian-iconize",
+        page = page,
+    );
+    let client = reqwest::Client::new();
+    let response = client
+        .get(&request_url)
+        .header(USER_AGENT, "FlorianWoelki")
+        .header(ACCEPT, "application/vnd.github+json")
+        .send()
+        .await;
+    match response {
+        Ok(response) => {
+            println!("{:?}", response);
+            let resolved_response = response
+                .json::<Vec<Issue>>()
+                .await
+                .expect("something went wrong while parsing");
+
+            let issues_next_page = get_issues_wrapper(Some(page + 1)).await;
+
+            return resolved_response
+                .into_iter()
+                .chain(issues_next_page)
+                .collect();
+        }
+        Err(_) => {
+            return Vec::new();
+        }
+    }
+}
+
+async fn get_issue_reactions(issue_id: usize) -> Vec<IssueReaction> {
+    let request_url = format!(
+        "https://api.github.com/repos/{owner}/{repo}/issues/{issue_id}/reactions",
+        owner = "FlorianWoelki",
+        repo = "obsidian-iconize",
+        issue_id = issue_id
     );
     let client = reqwest::Client::new();
     let response = client
@@ -26,15 +74,19 @@ async fn get_issue_reactions() -> Vec<Issue> {
         .await
         .expect("something went wrong while fetching");
     let resolved_response = response
-        .json::<Vec<Issue>>()
+        .json::<Vec<IssueReaction>>()
         .await
         .expect("something went wrong while parsing");
     resolved_response
 }
 
 fn main() {
-    let issues = tokio::runtime::Runtime::new()
-        .unwrap()
-        .block_on(get_issue_reactions());
-    println!("{:?}", issues);
+    let runtime = tokio::runtime::Runtime::new().unwrap();
+    let issues = runtime.block_on(get_issues(None));
+
+    for issue in &issues {
+        println!("Issue: {}", issue.title)
+    }
+
+    println!("Amount of issues: {}", issues.len())
 }
